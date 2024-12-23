42e85fb74d82fe204ea2a384a8ea1128b915df9f
oktavsm <incdmincs@gmail.com>
2024-12-23 17:02:16 +0700

    update grid size, input validation, and output

--- brute_force/bruteForceSudoku.java
+++ brute_force/bruteForceSudoku.java
@@ -1,7 +1,8 @@
-package belajar;
+
 import java.util.*;
 public class bruteForceSudoku {
-    static int grid[][] = new int[9][9];
+    static int grid[][];
+    static int gridSize;
     static int coordinates[][];
     static int blank = 0;
     public static final String RESET = "\u001B[0m";    // Mengembalikan warna ke default
@@ -10,13 +11,21 @@ public class bruteForceSudoku {
     static int solving =0;
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        for(int i=0;i<3*3;i++){
-            for(int j=0;j<3*3;j++){
+        gridSize= in.nextInt();
+        // input harus kuadrat sempurna
+        if (Math.sqrt(gridSize) % 1 != 0) {
+            System.err.println("Grid size must be a perfect square!");
+            return;
+        }
+        
+        grid = new int[gridSize][gridSize];
+        for(int i=0;i<gridSize;i++){
+            for(int j=0;j<gridSize;j++){
                 grid[i][j]=in.nextInt();
             }
         }
-        for(int i=0;i<3*3;i++){
-            for(int j=0;j<3*3;j++){
+        for(int i=0;i<gridSize;i++){
+            for(int j=0;j<gridSize;j++){
                 if(grid[i][j]==0){
                     blank++;
                 }
@@ -25,8 +34,8 @@ public class bruteForceSudoku {
         coordinates = new int[blank][2];
         int sumOfCoor=0;
         //catat koordinat field yang
-        for(int i=0;i<3*3;i++){
-            for(int j=0;j<3*3;j++){
+        for(int i=0;i<gridSize;i++){
+            for(int j=0;j<gridSize;j++){
                 if(grid[i][j]==0){
                     coordinates[sumOfCoor][0]=i;
                     coordinates[sumOfCoor][1]=j;
@@ -34,10 +43,13 @@ public class bruteForceSudoku {
                 }
             }
         }
-        
-        solve(0);
-        printGrid();
-        System.out.println(GREEN+String.format("Successfully solved with %d attempts",solving)+RESET);
+        if (!solve(0)) {
+            System.out.println(RED + "No solution found for the given Sudoku grid!" + RESET);
+        } else{
+            solve(0);
+            printGrid();
+            System.out.println(GREEN+String.format("Successfully solved with %d attempts",solving)+RESET);
+        }        
     }
     
     static boolean solve(int pos) {
@@ -47,16 +59,16 @@ public class bruteForceSudoku {
         }
 
         // rekursi untuk mencoba semua kemungkinan angka(1-9)
-        for (int i = 1; i <= 3*3; i++) {
+        for (int i = 1; i <= gridSize; i++) {
             grid[coordinates[pos][0]][coordinates[pos][1]] = i;
             solving++;
-            printGrid();
-            try {
-                Thread.sleep(10); // Jeda selama 1 detik
-            } catch (InterruptedException e) {
-                System.err.println("Thread terganggu: " + e.getMessage());
-            }
-            System.out.println(GREEN+"Solving..."+RESET);
+            // printGrid();
+            // try {
+            //     Thread.sleep(1); // Jeda selama 1 detik
+            // } catch (InterruptedException e) {
+            //     System.err.println("Thread terganggu: " + e.getMessage());
+            // }
+            // System.out.println(GREEN+"Solving..."+RESET);
             //cek untuk memastikan field sudah terisi dengan baik sebelum cek field selanjutnya
             if (horizontalCheck(coordinates[pos][0], coordinates[pos][1]) &&
                 verticalCheck(coordinates[pos][0], coordinates[pos][1]) &&
@@ -68,12 +80,12 @@ public class bruteForceSudoku {
             }
             grid[coordinates[pos][0]][coordinates[pos][1]] = 0; // Backtrack
         }
-        //setelah iterasi ternyata tidak menghasilkan nilai terbaik, maka kembalikan false ke pemanggilan sebelumnya
+        //setelah iterasi ternyata tidak menghasilkan nilai terbaik, maka kembalikan false ke pemanggilan sebelumnya dan field yang sebelumnya terisi di backtranking
         return false;
     }
 
     static boolean horizontalCheck(int row,int column){
-        for(int i=0;i<3*3;i++){
+        for(int i=0;i<gridSize;i++){
             if(grid[row][i]==grid[row][column]&&i!=column){
                 return false;
             }
@@ -82,7 +94,7 @@ public class bruteForceSudoku {
     }
     
     static boolean verticalCheck(int row, int column){
-        for(int i=0;i<3*3;i++){
+        for(int i=0;i<gridSize;i++){
             if(grid[i][column]==grid[row][column]&&i!=row){
                 return false;
             }
@@ -90,9 +102,9 @@ public class bruteForceSudoku {
         return true;
     }
     static boolean squareCheck(int row,int column){
-
-        for(int i=3*(row/3);i<3*(row/3+1);i++){
-            for(int j=3*(column/3);j<3*(column/3+1);j++){
+        int square = (int)Math.sqrt(gridSize);
+        for(int i=square*(row/square);i<square*(row/square+1);i++){
+            for(int j=square*(column/square);j<square*(column/square+1);j++){
                 if(grid[i][j]==grid[row][column]&&i!=row&&j!=column){
                     return false;
                 }
@@ -104,21 +116,33 @@ public class bruteForceSudoku {
     static void printGrid(){
         clearScreen();
         int pos = 0;
-        for(int i = 0;i<3*3;i++){
-            for(int j = 0;j<3*3;j++){
+        for(int i = 0;i<gridSize;i++){
+            for(int j = 0;j<gridSize;j++){
                 if(pos<coordinates.length){
                     if(i==coordinates[pos][0]&&j==coordinates[pos][1]&&grid[i][j]!=0){
-                        System.out.print(GREEN+grid[i][j]+" "+RESET);
+                        if(grid[i][j]>9){
+                            System.out.printf("%s%4c%s",GREEN,grid[i][j]+55,RESET);
+                        } else{
+                            System.out.printf("%s%4d%s",GREEN,grid[i][j],RESET);
+                        }
                         pos++;
                     }
                     else if (grid[i][j]==0){
-                        System.out.print(RED+grid[i][j]+" "+RESET);
+                        System.out.printf("%s%4d%s",RED,grid[i][j],RESET);
                     }
                     else{
-                        System.out.print(grid[i][j]+" ");
+                        if(grid[i][j]>9){
+                            System.out.printf("%4c",grid[i][j]+55);
+                        } else{
+                            System.out.printf("%4d",grid[i][j]);
+                        }
                     }
                 }else{
-                    System.out.print(grid[i][j]+" ");
+                    if(grid[i][j]>9){
+                        System.out.printf("%4c",grid[i][j]+55);
+                    } else{
+                        System.out.printf("%4d",grid[i][j]);
+                    }
                 }  
                 
                 
